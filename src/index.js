d38138acfa0bfe73e63c0f39d67d80508c070bf0
richard <richard.wasonga@student.moringaschool.com>
2025-03-31 18:57:46 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const input = document.querySelector("#new-task-description");
+    const task = input.value;
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = task;
+    taskList.appendChild(taskItem);
+
+    form.reset();
+  });
+
 });
